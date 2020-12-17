ef846e17a9cdfefe16b4166cab14245880a24d47
weilining <18277278902@163.com>
2020-12-17 10:54:01 +0800

    ripples

---
+++ jQuery/Ripples/butterfly@3.4.1.ripples.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  try {
+    $("#page-header").ripples({
+      resolution: 512,
+      dropRadius: 10, //px
+      perturbance: 0.04,
+    });
+    $("body").ripples({
+      resolution: 512,
+      dropRadius: 10, //px
+      perturbance: 0.04,
+    });
+  } catch (e) {
+    $(".error").show().text(e);
+  }
+});
