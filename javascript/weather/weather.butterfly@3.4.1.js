f079991fd4665c27d961e5c2f25d44a7478f7232
weilining <18277278902@163.com>
2020-12-18 13:01:50 +0800

    bash

---
+++ javascript/weather/weather.butterfly@3.4.1.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  CONFIG: {
+    layout: 2,
+    width: "230",
+    height: "275",
+    background: 5,
+    dataColor: "4A4A4A",
+    borderRadius: 5,
+    key: "WXnNsJZg8e",
+  },
+};
+(function (d) {
+  var c = d.createElement("link");
+  c.rel = "stylesheet";
+  c.href =
+    "https://apip.weatherdt.com/standard/static/css/weather-standard.css?v=2.0";
+  var s = d.createElement("script");
+  s.src =
+    "https://apip.weatherdt.com/standard/static/js/weather-standard.js?v=2.0";
+  var sn = d.getElementsByTagName("script")[0];
+  sn.parentNode.insertBefore(c, sn);
+  sn.parentNode.insertBefore(s, sn);
+})(document);
