cedad24cc58d382f928829f10f9f858dd097a4eb
weilining <18277278902@163.com>
2021-01-11 15:35:39 +0800

    bash

---
+++ javascript/music/music_playlist.js
@@ -0,0 +1,55 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [
+        {
+        name: '借 (Live)',
+        artist: '毛不易',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YXRNX726DXQHJHKGKGL3TUBPZAB',
+        cover: 'https://p1.music.126.net/yZN047VYz7selK4LCsi1Zg==/109951164681443868.jpg?param=130y130'
+        },
+        {
+        name: 'Feel You',
+        artist: '악의 꽃',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YUIU767YGCJ7ZBLXHAB7HSLVEVO',
+        cover: 'https://p2.music.126.net/UfY_VXNzPgGqbhddKV2vmQ==/109951165296168040.jpg?param=130y130'
+        },
+        {
+        name: 'Dream It Possible 曙光版',
+        artist: '华为',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YRLDQGKBRWRBBGZPC5JPZKXF3Y6',
+        cover: 'https://p2.music.126.net/Gfq_eVuq7lgilBf0w1g5dg==/109951163088676710.jpg?param=130y130'
+        },
+        {
+        name: '夜曲',
+        artist: '周杰伦',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YREMJSVGNNUOZEJYDDTSHHLLPWT',
+        cover: 'https://cdnmusic.migu.cn/picture/2019/1125/1028/AS001cc6d843be4301b73ae817ed07b8a4.jpg'
+        },
+        {
+        name: '给我一首歌的时间',
+        artist: '周杰伦',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YRPQX7EPLL3XZDJ7LBEIZFIMCTZ',
+        cover: 'https://cdnmusic.migu.cn/picture/2019/1031/0116/AS3f169c1d25854897bed1453a6aead54c.jpg'
+        },
+        {
+        name: '挪威的森林(Live)',
+        artist: '伍佰 & China Blue',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YWK2TBFMNTH65FZ6GTMU2NFBX7X',
+        cover: 'https://p2.music.126.net/6Lod8Ys5E6fiKZ9xrqCXug==/109951163100347037.jpg?param=130y130'  
+        },
+        {
+        name: '只是太爱你',
+        artist: '丁芙妮',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YS7UOEAJNAWZFA2ILFOZFKWFLR7',
+        cover: 'https://p1.music.126.net/mKhOfpO72lu1vrXbXohX8A==/109951163988545723.jpg?param=130y130'  
+        },
+        {
+        name: '说好的幸福呢',
+        artist: '周杰伦',
+        url: 'https://1368192376486968.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/oneindex-serverless-zxx/oneindex-serverless-zxx/download?id=01B7VK5YTF3S6NW4CRGRHYQWHWBJCIUHGK',
+        cover: 'https://cdnmusic.migu.cn/picture/2019/1031/0116/AS3f169c1d25854897bed1453a6aead54c.jpg'  
+        }
+        
+
+        ]
+});
